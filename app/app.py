53649691d99e890f9cc7eaf334814d9f0d41de18
billparker <bill.parker@neuralocity.com>
2020-03-12 06:25:34 -0700

    Redirect back to main page

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
@@ -19,7 +19,8 @@ def scrape():
     mars = mongo.db.mars
     mars_data = scrape_mars.scrape_all()
     mars.replace_one({}, mars_data, upsert=True)
-    return "Scraping Successful!"
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -4,6 +4,6 @@
 
 1) Python Jupyter notebook that scraps NASA web site
 
-2) Python Flask Web app that scraps NASA web site and store the data
-in a Mongo DB and can pull the data out of the db and display it 
+2) Python Flask Web app that scraps the NASA web site and store the data
+in a Mongo DB and can pull the data out of the db and displays it on the Web site. 
 
